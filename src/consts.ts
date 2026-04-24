0fa8a03e84f4fb860f33f1a84673c1a35821a16b
Kosuke Suzuki <kosukesuzuki@KosukenoMacBook-Air.local>
2026-04-24 11:32:49 +0900

    トップページ更新・プロフィール画像追加

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = '北のお菓子ものがたり';
+export const SITE_DESCRIPTION = '〜北海道のお菓子・食・畑の記録〜';
